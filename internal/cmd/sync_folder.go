ccbc950fd91901ee262c50147b303173039f43f2
David Alpert <david@spinthemoose.com>
2023-01-18 21:11:21 -0600

    refactor: extract common cfgset merge logic

--- internal/cmd/sync_folder.go
+++ internal/cmd/sync_folder.go
@@ -3,23 +3,20 @@ package cmd
 import (
 	"fmt"
 	"github.com/davidalpert/go-deep-merge/internal/app"
-	v1 "github.com/davidalpert/go-deep-merge/v1"
+	"github.com/davidalpert/go-deep-merge/internal/cfgset"
 	"github.com/davidalpert/go-printers/v1"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 	"os"
 	"path"
-	"sort"
-	"strings"
 )
 
 type SyncFolderOptions struct {
 	*printers.PrinterOptions
-	SourceFolder string
-	OutFolder    string
-	OutFormat    string
-	Debug        bool
+	cfgset.MergeOptions
+	OutFolder string
+	OutFormat string
 }
 
 func NewSyncFolderOptions(ioStreams printers.IOStreams) *SyncFolderOptions {
@@ -48,6 +45,7 @@ func NewCmdSyncFolder(ioStreams printers.IOStreams) *cobra.Command {
 	}
 
 	o.PrinterOptions.AddPrinterFlags(cmd.Flags())
+
 	cmd.Flags().BoolVarP(&o.Debug, "debug", "d", false, "enable debug output")
 	cmd.Flags().StringVar(&o.OutFolder, "out-folder", "out", "folder to place output")
 	//cmd.Flags().StringVar(&o.OutFormat, "out-format", "out", "format for output")
@@ -66,91 +64,11 @@ func (o *SyncFolderOptions) Validate() error {
 	return o.PrinterOptions.Validate()
 }
 
-type AppResult struct {
-	AppDir      string
-	MergeBySlug map[string]map[string]interface{}
-}
-
 // Run the command
 func (o *SyncFolderOptions) Run() error {
-	fis, err := afero.ReadDir(app.Fs, o.SourceFolder)
+	result, err := cfgset.Merge(o.MergeOptions)
 	if err != nil {
-		return fmt.Errorf("reading source folder %#v: %#v", o.SourceFolder, err)
-	}
-
-	appDirs := make([]string, 0)
-	for _, fi := range fis {
-		if fi.IsDir() {
-			appDirs = append(appDirs, path.Join(o.SourceFolder, fi.Name()))
-		}
-	}
-
-	result := make([]AppResult, 0)
-	for _, appDir := range appDirs {
-		var defaultFile string
-		var overrideFiles = make([]string, 0)
-
-		fis, err = afero.ReadDir(app.Fs, appDir)
-		for _, fi := range fis {
-			name := fi.Name()
-			if strings.HasSuffix(name, "default.yaml") {
-				defaultFile = path.Join(appDir, name)
-			} else if strings.HasSuffix(name, ".yaml") {
-				overrideFiles = append(overrideFiles, path.Join(appDir, name))
-			}
-		}
-
-		sort.Slice(overrideFiles, func(i, j int) bool {
-			return len(overrideFiles[i]) < len(overrideFiles[j])
-		})
-
-		destFile, err := afero.ReadFile(app.Fs, defaultFile)
-		if err != nil {
-			return fmt.Errorf("read dest file %#v: %#v", defaultFile, err)
-		}
-
-		mergeResultBySlug := make(map[string]map[string]interface{})
-		for _, override := range overrideFiles {
-			var dest map[string]interface{}
-			if err := yaml.Unmarshal(destFile, &dest); err != nil {
-				return fmt.Errorf("unmarshalling dest: %#v", err)
-			}
-
-			slug := strings.TrimSuffix(path.Base(override), path.Ext(override))
-			if strings.ContainsAny(slug, ".") {
-				baseSlug := strings.Split(slug, ".")[0]
-				// merge on top of another
-
-				r, err := v1.MergeWithOptions(mergeResultBySlug[baseSlug], dest, v1.NewConfigDeeperMergeBang().WithMergeHashArrays(true).WithDebug(o.Debug))
-				if err != nil {
-					return fmt.Errorf("merging files %#v -> %#v: %#v", override, defaultFile, err)
-				}
-
-				dest = r
-			}
-
-			sourceFile, err := afero.ReadFile(app.Fs, override)
-			if err != nil {
-				return fmt.Errorf("read source file %#v: %#v", override, err)
-			}
-
-			var src map[string]interface{}
-			if err := yaml.Unmarshal(sourceFile, &src); err != nil {
-				return fmt.Errorf("unmarshalling src: %#v", err)
-			}
-
-			r, err := v1.MergeWithOptions(src, dest, v1.NewConfigDeeperMergeBang().WithMergeHashArrays(true).WithDebug(o.Debug))
-			if err != nil {
-				return fmt.Errorf("merging files %#v -> %#v: %#v", override, defaultFile, err)
-			}
-
-			mergeResultBySlug[slug] = r
-		}
-
-		result = append(result, AppResult{
-			path.Base(appDir),
-			mergeResultBySlug,
-		})
+		return err
 	}
 
 	if err = app.Fs.MkdirAll(o.OutFolder, os.ModePerm); err != nil {
